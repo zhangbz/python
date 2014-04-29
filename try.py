09333826e79803b2b2889cdc8d09b76fe777166d
zhangbz <zhangbz.happyhacking@gmail.com>
2014-04-29 23:14:47 +0800

    try...except..finally

---
+++ try.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+def foo(s):
+    return 10 / int(s)
+
+def bar(s):
+    return foo(s) * 2
+
+def main():
+    try:
+        bar('0')
+    except StandardError, e:
+        print 'Error!'
+    finally:
+        print 'finally...'
+
+#main()
