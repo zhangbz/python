38bedc801978dfb7f0094fc324fe884db02e0b87
zhangbz <zhangbz.happyhacking@gmail.com>
2014-04-30 22:31:34 +0800

    json.loads object_hook = dict2student

--- json_test.py
+++ json_test.py
@@ -16,5 +16,11 @@ def student2dict(std):
         'score' : std.score
     }
 
+def dict2student(d):
+    return Student(d['name'], d['age'], d['score'])
+
 s = Student('Bob', 20, 88)
 print (json.dumps(s, default = lambda obj : obj.__dict__))
+
+json_str = '{"age" : 20, "score" : 88, "name" : "Bob"}'
+print (json.loads(json_str, object_hook = dict2student))
