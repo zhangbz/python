2ac8504188bbf653f494be8269ae75f7260dcf82
zhangbz <zhangbz.happyhacking@gmail.com>
2014-05-02 07:44:31 +0800

    step1:

--- Rabbits_and_badgers/game.py
+++ Rabbits_and_badgers/game.py
@@ -12,16 +12,20 @@ width,height = 640, 480
 screen = pygame.display.set_mode((width, height))
 keys = [False, False, False, False]
 playerpos = [100, 100]
+acc = [0, 0]
+arrows = []
 
 #3 - Load images
 player = pygame.image.load("resources/images/dude.png")
 grass = pygame.image.load("resources/images/grass.png")
 castle = pygame.image.load("resources/images/castle.png")
+arrow = pygame.image.load("resources/images/bullet.png")
 
 #4 - keep looping through
 while 1:
     #5 - clear the screen before drawing it again
     screen.fill(0)
+
     #6 - draw the screen elements
     for x in range(width/grass.get_width()+1):
         for y in range(height/grass.get_height()+1):
@@ -31,12 +35,26 @@ while 1:
     screen.blit(castle, (0, 240))
     screen.blit(castle, (0, 345))
     #screen.blit(player, playerpos)
+
     #6.1 - Set player position and rotation
     position = pygame.mouse.get_pos()
     angle = math.atan2(position[1] - (playerpos[1]+32), position[0] - (playerpos[0] + 26))
     playerrot = pygame.transform.rotate(player, 360 - angle * 57.29)
     playerpos1 = (playerpos[0] - playerrot.get_rect().width / 2, playerpos[1] - playerrot.get_rect().height / 2)
     screen.blit(playerrot, playerpos1)
+    #6.2 - Draw arrows
+    for bullet in arrows:
+        index = 0
+        velx = math.cos(bullet[0]) * 10 
+        vely = math.sin(bullet[0]) * 10 
+        bullet[1] += velx
+        bullet[2] += vely
+        if bullet[1] < -64 or bullet[1] > 640 or bullet[2] < -64 or bullet[2] > 480:
+            arrows.pop(index)
+        index += 1
+        for projectile in arrows:
+            arrow1 = pygame.transform.rotate(arrow, 360 - projectile[0] * 57.29)
+            screen.blit(arrow1,(projectile[1], projectile[2]))
 
     #7 - update the screen
     pygame.display.flip()
@@ -47,6 +65,7 @@ while 1:
             # if it is quit the game
             pygame.quit()
             exit(0)
+
         if event.type == pygame.KEYDOWN:
             if event.key == K_w:
                 keys[0] = True
@@ -56,6 +75,7 @@ while 1:
                 keys[2] = True
             elif event.key == K_d:
                 keys[3] = True
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_w:
                 keys[0] = False
@@ -65,6 +85,12 @@ while 1:
                 keys[2] = False
             elif event.key == pygame.K_d:
                 keys[3] = False
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            position = pygame.mouse.get_pos()
+            acc[1] += 1
+            arrows.append([math.atan2(position[1] - (playerpos1[1] + 32), position[0] - (playerpos1[0] + 26)), playerpos1[0] + 32, playerpos1[1] + 32])
+
     #9 - Move player
     if keys[0]:
         playerpos[1] -= 5
