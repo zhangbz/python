f1acd22a1df1c6b9825662cc6ef1401fa963de36
zhangbz <zhangbz.happyhacking@gmail.com>
2014-05-03 10:30:04 +0800

    time

--- Django/mysite/mysite/urls.py
+++ Django/mysite/mysite/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from mysite.views import hello
+from mysite.views import hello, current_datetime
 from django.contrib import admin
 admin.autodiscover()
 
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url('^hello/$', hello),
+    url('^time/$', current_datetime),
     url(r'^admin/', include(admin.site.urls)),
 )
