336d42e52380164404aefba1b3ad8a0ab3949990
zhangbz <zhangbz.happyhacking@gmail.com>
2014-05-04 23:47:37 +0800

    templates

--- Django/mysite/mysite/views.py
+++ Django/mysite/mysite/views.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-from django.template import Template, Context
+#from django.template.loader import get_template
+#from django.template import Context
 from django.http import Http404, HttpResponse
+from django.shortcuts import render_to_response
 import datetime
 
 def hello(request):
     return HttpResponse("Hello world")
 
 def current_datetime(request):
-    now = datetime.datetime.now()
-    #Simple way of using templates from the filesystem.
-    #This is BAD because it doesn't account for missing  files!
-    fp = open('/root/python/Django/mysite/mysite/templates/mytemplate.html')
-    t = Template(fp.read())
-    fp.close()
-    html = t.render(Context({'current_date': now}))
-    return HttpResponse(html)
+    current_date = datetime.datetime.now()
+    #t = get_template('current_datetime.html')
+    #html = t.render(Context({'current_date': now}))
+    #return HttpResponse(html)
+    return render_to_response('current_datetime.html', locals())
 
 def hours_ahead(request, offset):
     try:
-       offset = int(offset)
+       hours_offset = int(offset)
     except ValueError:
         raise Http404()
-    dt = datetime.datetime.now() + datetime.timedelta(hours = offset)
+    next_time = datetime.datetime.now() + datetime.timedelta(hours = hours_offset)
     assert True
-    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-    return HttpResponse(html)
+    #html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    #return HttpResponse(html)
+    return render_to_response('hours_ahead.html',locals())
