b3ef9fe93bf8a9b4fa4afcdff8c0cde216e324c0
zhangbz <zhangbz.happyhacking@gmail.com>
2014-05-06 23:52:37 +0800

    add books to admin

--- Django/mysite/mysite/books/admin.py
+++ Django/mysite/mysite/books/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from mysite.books.models import Publisher, Author, Book
 
 # Register your models here.
+admin.site.register(Publisher)
+admin.site.register(Author)
+admin.site.register(Book)
