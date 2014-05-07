8ab2d2660d916ea5b1707b514c256f96b0d584c0
zhangbz <zhangbz.happyhacking@gmail.com>
2014-05-07 21:26:38 +0800

    display_meta

--- Django/mysite/mysite/urls.py
+++ Django/mysite/mysite/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from mysite.views import hello, current_datetime, hours_ahead
+from mysite.views import hello, current_datetime, hours_ahead, display_meta
 from django.contrib import admin
 admin.autodiscover()
 
@@ -11,4 +11,5 @@ urlpatterns = patterns('',
     url('^time/$', current_datetime),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+    url(r'^display_meta/$', display_meta),
 )
--- Django/mysite/mysite/views.py
+++ Django/mysite/mysite/views.py
@@ -27,3 +27,11 @@ def hours_ahead(request, offset):
     #html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
     #return HttpResponse(html)
     return render_to_response('hours_ahead.html',locals())
+
+def display_meta(request):
+    values = request.META.items()
+    values.sort()
+    html = []
+    for k, v in values:
+        html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
+    return HttpResponse('<table>%s</table>' % '\n'.join(html))
