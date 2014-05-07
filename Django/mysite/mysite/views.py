04df0e175b61c3e6d16e436d7847a2c22644b867
zhangbz <zhangbz.happyhacking@gmail.com>
2014-05-07 22:32:31 +0800

    note

--- Django/mysite/mysite/views.py
+++ Django/mysite/mysite/views.py
@@ -29,6 +29,7 @@ def hours_ahead(request, offset):
     return render_to_response('hours_ahead.html',locals())
 
 def display_meta(request):
+#做为一个练习，看你自己能不能把上面这个view函数改用Django模板系统来实现，而不是上面这样来手动输入HTML代码。 也可以试着把前面提到的 request.path 方法或 HttpRequest 对象的其它方法加进去。
     values = request.META.items()
     values.sort()
     html = []
