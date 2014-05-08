cf6ae681c3a7bb5b6a646fd799115995dfb6b337
zhangbz <zhangbz.happyhacking@gmail.com>
2014-05-08 23:36:45 +0800

    add urls

--- Django/mysite/mysite/urls.py
+++ Django/mysite/mysite/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from mysite.views import hello, current_datetime, hours_ahead, display_meta
 from django.contrib import admin
+from mysite.books import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -12,4 +13,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^time/plus/(\d{1,2})/$', hours_ahead),
     url(r'^display_meta/$', display_meta),
+    url(r'^search-form/$', views.search_form),
+    url(r'^search/$', views.search),
 )
