84d75059e4012250244627bb0a3c301b0af79295
zhangbz <zhangbz.happyhacking@gmail.com>
2014-08-09 20:54:12 +0800

    changes

--- ml/treePlotter.py
+++ ml/treePlotter.py
@@ -52,3 +52,5 @@ def getTreeDepth(myTree):
 def retrieveTree(i):
     listOfTrees = [{'no surfacing':{0:'no', 1:{'flippers':{0:'no', 1:'yes'}}}}, {'no surfacing':{0:'no', 1:{'flippers':{0:{'head':{0:'no', 1:'yes'}},1:'no'}}}}]
     return listOfTrees[i]
+
+
