ec4ff6a40a1390d211ccd758e2e46338a66beedd
zhangbz <zhangbz.happyhacking@gmail.com>
2014-08-10 22:24:45 +0800

    Form class

--- Django/mysite/mysite/contact/views.py
+++ Django/mysite/mysite/contact/views.py
@@ -1,10 +1,21 @@
 from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
+from mysite.contact.forms import ContactForm
 
 def contact(request):
-    errors = []
+    #errors = []
     if request.method == 'POST':
+        form = ContactForm(requst.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            send_mial(
+                 cd['subject'],
+                 cd['message'],
+                 cd.get('email', 'noreply@example.com'),
+                 ['siteowner@example.com'],
+            )
+"""
         if not request.POST.get('subject', ''):
             errors.append('Enter a subject.')
         if not request.POST.get('message', ''):
@@ -17,5 +28,12 @@ def contact(request):
                 request.POST['message'],
                 request.POST.get('email', 'noreply@example.com'),['siteowner@example.com'],
                 )
+"""
             return HttpResponseRedirect('/contact/thanks')
-    return render_to_response('contact_form.html', {'errors': errors, 'subject':request.POST.get('subject', ''), 'message': request.POST.get('message', ''), 'email': request.POST.get('email', ''), })
+    else:
+        form = ContactForm(initial={'subject': 'I love your site!'})
+     return render_to_response('contact_form.html', {'form': form})
+"""    
+     return render_to_response('contact_form.html', {'errors': errors, 'subject':request.POST.get('subject', ''), 'message': request.POST.get('message', ''), 'email': request.POST.get('email', ''), })
+"""
+
--- Django/mysite/mysite/urls.py
+++ Django/mysite/mysite/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from mysite.views import hello, current_datetime, hours_ahead, display_meta
 from django.contrib import admin
 from mysite.books import views
+from mysite.contact import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
